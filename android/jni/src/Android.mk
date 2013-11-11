21da83360a3b871400ad82ed9b18c76c76e3a3b0
Carles Pagès <page@cubata.homelinux.net>
2013-11-11 22:57:48 +0100

    Start with main.cpp, and enable stl and exceptions.
    I started by only adding main.cpp file. I enabled all the c++ stuff that is
    disabled by default in android builds. To start testing, I simlinked SDL in jni
    folder.

--- android/jni/src/Android.mk
+++ android/jni/src/Android.mk
@@ -2,15 +2,19 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_CPP_FEATURES += exceptions
+
 LOCAL_MODULE := main
 
 SDL_PATH := ../SDL
+
+OXC_PATH := ../../../src
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
 
 # Add your application source files here...
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
-	YourSourceHere.c
+	$(OXC_PATH)/main.cpp
 
 LOCAL_SHARED_LIBRARIES := SDL2
 
