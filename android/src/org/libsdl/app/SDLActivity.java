ff7eae4eae5ba63eaf0258fbc489c9d4383e9a9c
Carles Pagès <page@cubata.homelinux.net>
2013-11-15 23:57:50 +0100

    android: load sdl companion libs.

--- android/src/org/libsdl/app/SDLActivity.java
+++ android/src/org/libsdl/app/SDLActivity.java
@@ -44,8 +44,9 @@ public class SDLActivity extends Activity {
     // Load the .so
     static {
         System.loadLibrary("SDL2");
-        //System.loadLibrary("SDL2_image");
-        //System.loadLibrary("SDL2_mixer");
+        System.loadLibrary("SDL2_image");
+        System.loadLibrary("SDL2_mixer");
+        System.loadLibrary("SDL_gfx");
         //System.loadLibrary("SDL2_net");
         //System.loadLibrary("SDL2_ttf");
         System.loadLibrary("main");
