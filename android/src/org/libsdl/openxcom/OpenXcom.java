309e659c607168483ccba6c1b06bf420cfddebb3
sfalexrog <sfalexrog@gmail.com>
2014-10-12 00:46:31 +0400

    Add a method stub for reading directories.

--- android/src/org/libsdl/openxcom/OpenXcom.java
+++ android/src/org/libsdl/openxcom/OpenXcom.java
@@ -1,6 +1,9 @@
 package org.libsdl.openxcom;
 
 import org.libsdl.app.SDLActivity;
+import android.util.Log;
+import android.content.res.Configuration;
+import android.content.pm.ActivityInfo;
 
 /*
  * Just a wrapper, I guess
@@ -8,4 +11,20 @@ import org.libsdl.app.SDLActivity;
  */
 
 
-public class OpenXcom extends SDLActivity { };
+public class OpenXcom extends SDLActivity { 
+	public static void showDirDialog() {
+		Log.i("OpenXcom", "Hello from showDirDialog()!");
+
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+			newConfig.orientation = Configuration.ORIENTATION_LANDSCAPE;
+		}
+
+		super.onConfigurationChanged(newConfig);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+	}
+
+};
