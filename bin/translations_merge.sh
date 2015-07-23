a62456a44ba3e1093afb6b324ad7c440349a5d66
sfalexrog <sfalexrog@gmail.com>
2015-07-23 20:03:01 +0300

    Modify translation adding system to accomodate for newer translation system.

--- bin/translations_merge.sh
+++ bin/translations_merge.sh
@@ -2,10 +2,25 @@
 
 # This line is taken from https://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 DATA_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-PROJECT=x-com-1
-SOURCES_DIR=$DATA_DIR/translations/$PROJECT/sources
-ADDITIONAL_DIR=$DATA_DIR/translations/$PROJECT/additional
-OUTPUT_DIR=$DATA_DIR/translations/$PROJECT/output
+# So far, only the common elements have system-specific translations
+PROJECT=common
+
+TRANSLATIONS_SOURCE=$DATA_DIR/translations/sources
+TRANSLATIONS_ADDITIONAL=$DATA_DIR/translations/additional
+TRANSLATIONS_OUTPUT=$DATA_DIR/translations/output
+
+SOURCES_DIR=$TRANSLATIONS_SOURCE/$PROJECT/Language
+ADDITIONAL_DIR=$TRANSLATIONS_ADDITIONAL/$PROJECT/Language
+OUTPUT_DIR=$TRANSLATIONS_OUTPUT/$PROJECT/Language
+
+echo "sources dir is $SOURCES_DIR"
+echo "additional dir is $ADDITIONAL_DIR"
+echo "output dir is $OUTPUT_DIR"
+
+if [ ! -f $OUTPUT_DIR ]; then
+	# create output dir if it doesn't exist
+	mkdir -p $OUTPUT_DIR
+fi
 
 for i in $SOURCES_DIR/*.yml;
 do
@@ -19,3 +34,9 @@ do
     cp $SOURCES_DIR/$file $OUTPUT_DIR/$file
   fi
 done
+
+echo "Carbon-copying standard files - they don't have system-specific translations."
+
+cp -r $TRANSLATIONS_SOURCE/standard/ $TRANSLATIONS_OUTPUT/standard/
+
+echo "All done."
